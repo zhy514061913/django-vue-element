d9be314ef32dc2e60fb669459d142d042397da2b
zhy514061913 <514061913@qq.com>
2019-11-03 15:35:11 +0800

    first commit

---
+++ frontend/src/utils/auth.js
@@ -0,0 +1,17 @@
+/**
+ * author:laoseng(QQ:1572665580),feilong(hhr66@qq.com)
+ * create:2018-07
+ *  cookie 设置
+ */
+import Cookies from 'js-cookie'
+// var param = { path: '/', domain: document.domain.match(/[^\.]+\.[^\.]+$/)[0] }
+var param = { path: '/', domain: 'localhost:9527' }
+
+export function getToken() {
+  var tokenName = Cookies.get('token_name', param)
+  if (tokenName) {
+    return Cookies.get(tokenName, param)
+  } else {
+    return false
+  }
+}
